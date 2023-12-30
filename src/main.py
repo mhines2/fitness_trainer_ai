d11c86936b9de9843a68085afcdbcb53bb6b223a
Michael Hines <mhines2@nd.edu>
2023-12-30 15:36:37 -0500

    main.py: complete

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
-import openai
+#!/usr/bin/env python3
+
+import os
+from openai import OpenAI
 import gradio
 
-openai.api_key = "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"
+api_key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
 messages = [{"role": "system", "content": "You are a personal fitness trainer specializing in health and wellness."}]
 
 def CustomFitnessAssistant(user_input):
     messages.append({"role": "user", "content": user_input})
-    response = openai.ChatCompletion.create(
+    
+    chat_completion = client.chat.completions.create(
+        messages=messages,
         model="gpt-3.5-turbo",
-        messages=messages
     )
-    fitness_assistant_reply = response["choices"][0]["message"]["content"]
+    
+    fitness_assistant_reply = chat_completion["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": fitness_assistant_reply})
+    
     return fitness_assistant_reply
 
 demo = gradio.Interface(fn=CustomFitnessAssistant, inputs="text", outputs="text", title="Fitness Trainer AI")
